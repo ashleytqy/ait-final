1bd5fc96a92157813faeba44317a0e01354bda87
Ashley Teoh <atq203@nyu.edu>
2017-04-15 19:15:00 -0400

    changed gitignore

---
+++ env.js
@@ -0,0 +1,4 @@
+process.env['CLIENT_ID'] = "203275700129678"
+process.env['CLIENT_SECRET'] = "b2b8ba0fcb7ff627750ee79d64c5f3bd"
+process.env['MONGO_USERNAME'] = "ashleytqy"
+process.env['MONGO_PASSWORD'] = "password123"
